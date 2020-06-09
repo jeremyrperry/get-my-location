9f850c8b8fb649bf1203029ec9728fe2a194136e
jeremyrperry <jperry@sesamecommunicaitons.com>
2020-06-09 01:02:33 -0700

    Performed re-work to ensure smooth experience on all devices.

--- js/get_my_location.js
+++ js/get_my_location.js
@@ -1,17 +1,22 @@
 
 
 const get_my_location = {
-    timeout: 10000,
+    timeout: null,
+
+    clearActiveTimeout(){
+        clearTimeout(this.timeout);
+    },
 
     getLocation(){
         $('#my_location_results').val('');
+        $('#copy-btn').hide();
+        $('.alert').hide();
+        this.clearActiveTimeout();
         if(navigator.geolocation){
             $('.alert-info').show();
             navigator.geolocation.getCurrentPosition((position)=>{
-                $('.alert-info').hide();
                 this.onLocationReceived(position)
             }, ()=>{
-                $('.alert-info').hide();
                 this.onLocationNotReceived();
             }, {
                 enableHighAccuracy: true
@@ -21,55 +26,41 @@ const get_my_location = {
             this.onLocationNotReceived();
         }
     },
-    
-    getSelectionIOS($textarea){
-        const editable = $textarea.contentEditable;
-        const readOnly = $textarea.readOnly;
-        $textarea.contentEditable = true;
-        $textarea.readOnly = true;
-        const range = document.createRange();
-        range.selectNodeContents($textarea);
-        const selection = window.getSelection();
-        selection.removeAllRanges();
-        selection.addRange(range);
-        $textarea.setSelectionRange(0, 999999);
-        $textarea.contentEditable = editable;
-        $textarea.readOnly = readOnly;
-    },
 
     init(){
-        $('form#get_my_location').submit((evt)=>{
-            evt.preventDefault();
+        const clipboard = new ClipboardJS('#copy-btn');
+        $('#get-location-btn').click((evt)=>{
             this.getLocation();
-        })
+        });
+        $('#copy-btn').click((evt)=>{
+            $('.alert').hide();
+            this.showAlert($('.alert-location-copied'));
+            $(evt.currentTarget).hide();
+            $('#get-location-btn').show();
+        });
     },
 
     onLocationNotReceived(){
-        $('.alert-warning').show();
-        setTimeout(()=>{
-            $('.alert-warning').hide();
-        }, this.timeout);
+        this.showAlert($('.alert-warning'));
     },
 
     onLocationReceived(position){
         const link = `https://maps.google.com/?q=${position.coords.latitude},${position.coords.longitude}`;
         const verbiage = `My location:\n\n${link}`;
-        const $textarea = document.querySelector('#my_location_results');
-        $textarea.value = verbiage;
-        if (navigator.userAgent.match(/ipad|ipod|iphone/i)) {
-            this.getSelectionIOS($textarea);
-        }
-        else{
-            $textarea.select();
-        }
-        document.execCommand('copy');
-        $('.alert-success').show();
-        setTimeout(()=>{
-            $('.alert-success').hide();
-        }, this.timeout);
+        $('#my_location_results').val(verbiage);
+        $('#copy-btn').show();
+        $('#get-location-btn').hide();
+        this.showAlert($('.alert-location-received'));
+    },
+
+    showAlert($alert){
+        $('.alert').hide();
+        $alert.show();
+        this.clearActiveTimeout();
+        this.timeout = setTimeout(()=>{
+            $alert.hide();
+        }, 10000);
     }
-
-
 };
 
 $(document).ready(()=>{
