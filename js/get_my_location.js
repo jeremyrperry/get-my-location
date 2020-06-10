ea244d27d97570f044d3edf1a1c01b7d981e402a
jeremyrperry <jperry@sesamecommunicaitons.com>
2020-06-10 00:13:04 -0700

    Expanded map type options.

--- js/get_my_location.js
+++ js/get_my_location.js
@@ -7,6 +7,28 @@ const get_my_location = {
         clearTimeout(this.timeout);
     },
 
+    getLink(position){
+        const lat = position.coords.latitude;
+        const lon = position.coords.longitude;
+        const option = $( "#choose_map option:selected" ).val();
+        switch(option){
+            case 'google':
+                return `https://maps.google.com/?q=${lat},${lon}`;
+                break;
+            case 'bing':
+                return `https://www.bing.com/maps?cp=${lat}~-${lon}&lvl=16&sp=point.${lat}_${lon}_My%20Location`;
+                break;
+            case 'apple':
+                return `http://maps.apple.com/?ll=4${lat},${lon}&q=My+Location`;
+                break;
+            case 'osm':
+                return `https://www.openstreetmap.org/?mlat=${lat}&mlon=${lon}&zoom=16`;
+                break;
+            default:
+                return `${lat},${lon}`;
+        }
+    },
+
     getLocation(){
         $('#my_location_results').val('');
         $('#copy-btn').hide();
@@ -36,7 +58,6 @@ const get_my_location = {
             $('.alert').hide();
             this.showAlert($('.alert-location-copied'));
             $(evt.currentTarget).hide();
-            $('#get-location-btn').show();
         });
     },
 
@@ -45,11 +66,10 @@ const get_my_location = {
     },
 
     onLocationReceived(position){
-        const link = `https://maps.google.com/?q=${position.coords.latitude},${position.coords.longitude}`;
+        const link = this.getLink(position);
         const verbiage = `My location:\n\n${link}`;
         $('#my_location_results').val(verbiage);
         $('#copy-btn').show();
-        $('#get-location-btn').hide();
         this.showAlert($('.alert-location-received'));
     },
 
